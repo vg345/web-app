24e9106bc025341ea146abccd63a336acab75736
vg345 <vanshikag345@gmail.com>
2018-12-07 09:03:50 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/')
+
+app.listen(port, ()=> {
+    console.log(`Server is up on port ${port}`)
+});
